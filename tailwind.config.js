7911ab751ffe7da266e56da47b50b9f83be1d171
omijack <omargabriel.k@gmail.com>
2025-07-13 16:05:08 +0200

    Inici portal hipotecari Mou

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+theme: {
+  extend: {
+    colors: {
+      primary: '#00B894',
+      secondary: '#005AA7',
+      neutral: {
+        100: '#f5f5f5',
+      },
+    },
+  },
+},
+
