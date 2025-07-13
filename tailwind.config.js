88981b857128f4d5a53186ce3ba7d8be36744783
omijack <omargabriel.k@gmail.com>
2025-07-13 17:35:07 +0200

    corregir

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
-content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-theme: {
-  extend: {
-    colors: {
-      primary: '#00B894',
-      secondary: '#005AA7',
-      neutral: {
-        100: '#f5f5f5',
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00B894',
+        secondary: '#005AA7',
+        neutral: {
+          100: '#f5f5f5',
+        },
       },
     },
   },
-},
-
+  plugins: [],
+};
